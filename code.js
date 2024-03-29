dac15936a2ac62fa4a3715f3cf3f49ef61fe7801
JacobMorgan3 <144738756+JacobMorgan3@users.noreply.github.com>
2024-03-29 15:44:19 -0600

    Update code.js

--- code.js
+++ code.js
@@ -9,29 +9,37 @@ function dijkstra(graph, sourceNode) {
     }
     
     dist[sourceNode] = 0; //set estimated dist to 0 for the sourceNode
-    
+    loopNumber = 1;
+    //console.log("befoere the first loop: " + dist);
     while (true) {
+        console.log("loop " + loopNumber);
         let lowest = 1000; //set lowest to our infinity
         let lowestIndex = 0;
         for (let i = 0; i < dist.length; i++) { //find the node with the lowest estimated dist that is not marked
-            for (let j = 0; j < unmarked.length; j++) {
+            for (let j = 0; j < marked.length; j++) {
                 if (marked[j] == false) { //if not marked
                     if (dist[i] < lowest) { //finding the lowest estimated dist 
                         lowest = dist[i];
                         lowestIndex = i;
+                        //console.log("i: " + i);
                     }   
                 }
             }
         }
         marked[lowestIndex] = true; //mark lowestIndex node
         distance[lowestIndex] = lowest; //set distance for the lowest dist to dist
-        
-        //look at all the edges that leave from lowestIndex
+    
+        //look at all the edges that leave lowestIndex and update there dist
         for (let i = 0; i < graph[lowestIndex].length; i++) {
-            
+            if (graph[lowest][i][1] < 1000) { //if dist is lower than 'infinity' update it
+                dist[graph[lowest][i][0]] = graph[lowest][i][1];
+            }
         }
         
-        
+        loopNumber++;
+        console.log("dist: " + dist);
+        console.log("distance: " + distance);
+        console.log("marked: " + marked);
         
     }
     
@@ -44,6 +52,19 @@ function dijkstra(graph, sourceNode) {
     
     return [];
 }
+//graph is from example in lecture videos
+graph = [ [[1,2],[2,1],[3,4]], //node A
+          [[2,1],[4,10],[5,2]], //node B
+          [[0,9],[4,8]], //node C
+          [[2,2]], //node D
+          [[3,7],[6,1]], //node E
+          [[5,3]], //node F
+          [[4,4],[5,2]], //node G
+          [[6,1]] ]; //node H
+          
+          
+sourceNode = 2;
+console.log(dijkstra(graph, sourceNode))
 
 
-graph = [ [[1,5],[4,2]], [[3,1]], [[0,3]], [[2,6]], [[1,2],[3,4]] ];
+
